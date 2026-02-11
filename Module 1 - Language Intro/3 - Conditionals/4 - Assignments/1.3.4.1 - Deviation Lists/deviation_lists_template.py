127db035b266e99e33af71f6c856b64e3590e272
kozurajr <kozurajr@dukes.jmu.edu>
2026-02-11 14:48:12 -0500

    2/11/26 3

--- Module 1 - Language Intro/3 - Conditionals/4 - Assignments/1.3.4.1 - Deviation Lists/deviation_lists_template.py
+++ Module 1 - Language Intro/3 - Conditionals/4 - Assignments/1.3.4.1 - Deviation Lists/deviation_lists_template.py
@@ -30,3 +30,16 @@ random_list_B = random.sample(range(100), random_length)
 longest_list_is = None
 
 ### YOUR CODE HERE
+
+sample_A = np.std(random_list_A)
+sample_B = np.std(random_list_B)
+largest_sample = "none"
+
+if sample_A > sample_B:
+    longest_list_is = sample_A
+    largest_sample = "List A"
+else:
+    longest_list_is = sample_B
+    largest_sample = "List B"
+
+print("The largest standard deviation is", largest_sample, longest_list_is)
